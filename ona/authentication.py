afc7443b3364036f55467fe61bde7384368572a3
ivermac <mark.ekisa@gmail.com>
2016-12-07 15:41:23 +0300

    ME: remove temptoken references

--- ona/authentication.py
+++ ona/authentication.py
@@ -10,7 +10,6 @@ from rest_framework.authentication import (
     TokenAuthentication, get_authorization_header
 )
 from rest_framework.authtoken.models import Token
-from ona.models import TempToken
 
 
 JWT_SECRET_KEY = getattr(settings, 'JWT_SECRET_KEY', 'jwt')
@@ -31,20 +30,6 @@ def decode_payload(payload):
     )
 
     return decoded_payload
-
-
-def expired(time_token_created):
-    """Checks if the time between when time_token_created and current time
-    is greater than the token expiry time.
-
-    :params time_token_created: The time the token we are checking was created.
-    :returns: Boolean True if not passed expired time, otherwise False.
-    """
-    temp_token_expiry_time = 21600  # 6 hours
-    time_diff = (timezone.now() - time_token_created).total_seconds()
-    token_expiry_time = temp_token_expiry_time
-
-    return True if time_diff > token_expiry_time else False
 
 
 def get_api_token(json_web_token):
@@ -100,41 +85,3 @@ class JWTAuthentication(TokenAuthentication):
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
-
-
-class TempTokenAuthentication(TokenAuthentication):
-    model = TempToken
-
-    def authenticate(self, request):
-        auth = get_authorization_header(request).split()
-
-        if not auth or auth[0].lower() != b'temptoken':
-            return None
-
-        if len(auth) == 1:
-            m = _(u'Invalid token header. No credentials provided.')
-            raise exceptions.AuthenticationFailed(m)
-        elif len(auth) > 2:
-            m = _(u'Invalid token header. '
-                  'Token string should not contain spaces.')
-            raise exceptions.AuthenticationFailed(m)
-
-        return self.authenticate_credentials(auth[1])
-
-    def authenticate_credentials(self, key):
-        try:
-            token = self.model.objects.get(key=key)
-        except self.model.DoesNotExist:
-            raise exceptions.AuthenticationFailed(_(u'Invalid token'))
-
-        if not token.user.is_active:
-            raise exceptions.AuthenticationFailed(
-                _(u'User inactive or deleted'))
-
-        if expired(token.created):
-            raise exceptions.AuthenticationFailed(_(u'Token expired'))
-
-        return (token.user, token)
-
-    def authenticate_header(self, request):
-        return 'TempToken'
