0a818c1d8cdbbc5b87e86c51ea3c00a34057709e
Olivier Leger <olivierleger@gmail.com>
2019-05-18 10:19:38 -0400

    Renamed 'viewset_mixin.py' to 'viewset_mixins.py' to be compliant with DRF

--- kobo/apps/hook/views/v2/hook.py
+++ kobo/apps/hook/views/v2/hook.py
@@ -14,58 +14,38 @@ from kobo.apps.hook.constants import HOOK_LOG_FAILED, HOOK_LOG_PENDING
 from kobo.apps.hook.models import Hook, HookLog
 from kobo.apps.hook.serializers.v2.hook import HookSerializer
 from kobo.apps.hook.tasks import retry_all_task
-<<<<<<< HEAD
 from kpi.permissions import AssetEditorSubmissionViewerPermission
 from kpi.utils.viewset_mixins import AssetNestedObjectViewsetMixin
-=======
-from kpi.filters import AssetOwnerFilterBackend
-from kpi.permissions import AssetOwnerNestedObjectPermission
-from kpi.utils.viewset_mixin import AssetNestedObjectViewsetMixin
->>>>>>> Working nested permissions serializer, refactored Asset nested View (to use AssetNestedMixin)
 
 
 class HookViewSet(AssetNestedObjectViewsetMixin, NestedViewSetMixin,
                   viewsets.ModelViewSet):
     """
-
     ## External services
-
     Lists the external services endpoints accessible to requesting user
-
     <pre class="prettyprint">
     <b>GET</b> /api/v2/assets/{asset_uid}/hooks/
     </pre>
-
     > Example
     >
     >       curl -X GET https://[kpi-url]/api/v2/assets/a9PkXcgVgaDXuwayVeAuY5/hooks/
-
     ## CRUD
-
     * `asset_uid` - is the unique identifier of a specific asset
     * `uid` - is the unique identifier of a specific external service
-
     #### Retrieves an external service
     <pre class="prettyprint">
     <b>GET</b> /api/v2/assets/<code>{asset_uid}</code>/hooks/<code>{uid}</code>
     </pre>
-
-
     > Example
     >
     >       curl -X GET https://[kpi-url]/api/v2/assets/a9PkXcgVgaDXuwayVeAuY5/hooks/hfgha2nxBdoTVcwohdYNzb
-
     #### Add an external service to asset.
     <pre class="prettyprint">
     <b>POST</b> /api/v2/assets/<code>{asset_uid}</code>/hooks/
     </pre>
-
-
     > Example
     >
     >       curl -X POST https://[kpi-url]/api/v2/assets/a9PkXcgVgaDXuwayVeAuY5/hooks/
-
-
     > **Payload to create a new external service**
     >
     >        {
@@ -87,73 +67,51 @@ class HookViewSet(AssetNestedObjectViewsetMixin, NestedViewSetMixin,
     >           },
     >           "payload_template": {string}
     >        }
-
     where
-
     * `name` and `endpoint` are required
     * `active` is True by default
     * `export_type` must be one these values:
-
         1. `json` (_default_)
         2. `xml`
-
     * `email_notification` is a boolean. If true, User will be notified when request to remote server has failed.
     * `auth_level` must be one these values:
-
         1. `no_auth` (_default_)
         2. `basic_auth`
-
     * `subset_fields` is the list of fields of the form definition. Only these fields should be present in data sent to remote server
     * `settings`.`custom_headers` is dictionary of `custom header`: `value`
-
     For example:
     >           "settings": {
     >               "customer_headers": {
     >                   "Authorization" : "Token 1af538baa9045a84c0e889f672baf83ff24"
     >               }
-
     * `payload_template` is a custom wrapper around `%SUBMISSION%` when sending data to remote server.
        It can be used only with JSON submission format.
-
     For example:
     >           "payload_template": '{"fields": %SUBMISSION%}'
-
     #### Update an external service.
     <pre class="prettyprint">
     <b>PATCH</b> /api/v2/assets/<code>{asset_uid}</code>/hooks/{uid}
     </pre>
-
-
     > Example
     >
     >       curl -X PATCH https://[kpi-url]/api/v2/assets/a9PkXcgVgaDXuwayVeAuY5/hooks/hfgha2nxBdoTVcwohdYNzb
-
-
     Only specify properties to update in the payload. See above for payload structure
-
     #### Delete an external service.
     <pre class="prettyprint">
     <b>DELETE</b> /api/v2/assets/<code>{asset_uid}</code>/hooks/{uid}
     </pre>
-
-
     > Example
     >
     >       curl -X DELETE https://[kpi-url]/api/v2/assets/a9PkXcgVgaDXuwayVeAuY5/hooks/hfgha2nxBdoTVcwohdYNzb
-
     #### Retries all failed attempts
     <pre class="prettyprint">
     <b>PATCH</b> /api/v2/assets/<code>{asset_uid}</code>/hooks/<code>{hook_uid}</code>/retry/
     </pre>
-
     **This call is asynchronous. Job is sent to Celery to be run in background**
-
     > Example
     >
     >       curl -X PATCH https://[kpi-url]/api/v2/assets/a9PkXcgVgaDXuwayVeAuY5/hooks/hfgha2nxBdoTVcwohdYNzb/retry/
-
     It returns all logs `uid`s that are being retried.
-
     ### CURRENT ENDPOINT
     """
 
@@ -166,7 +124,11 @@ class HookViewSet(AssetNestedObjectViewsetMixin, NestedViewSetMixin,
 
     def get_queryset(self):
         queryset = self.model.objects.filter(asset__uid=self.asset.uid)
-        queryset = queryset.select_related("asset__uid")
+        # Even though we only need 'uid', `select_related('asset__uid')`
+        # actually pulled in the entire `kpi_asset` table under Django 1.8. In
+        # Django 1.9, "select_related() prohibits non-relational fields for
+        # nested relations."
+        queryset = queryset.select_related('asset')
         return queryset
 
     def perform_create(self, serializer):
