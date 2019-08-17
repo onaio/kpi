e303e7fa63a05291911c10ef168416f66fa3fe3f
Olivier Leger <olivierleger@gmail.com>
2019-08-17 10:18:54 -0400

    Reorganized some imports and applied few PEP-8 rules

--- kpi/deployment_backends/kc_access/shadow_models.py
+++ kpi/deployment_backends/kc_access/shadow_models.py
@@ -433,6 +433,7 @@ class KobocatUserPermission(ShadowModel):
     """ Needed to assign model-level KoBoCAT permissions """
     user = models.ForeignKey('KobocatUser', db_column='user_id')
     permission = models.ForeignKey('KobocatPermission', db_column='permission_id')
+
     class Meta(ShadowModel.Meta):
         db_table = 'auth_user_user_permissions'
 
