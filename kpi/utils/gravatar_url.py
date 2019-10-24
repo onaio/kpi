802c635c18c0b9c6c6009d9d8c8a7a30cd58a498
Olivier Leger <olivierleger@gmail.com>
2019-10-23 23:13:26 -0300

    - JS: Read CSRF cookie each time a new ajax call is made - Updated pip dependencies for Python 3 only - Removed py-gfm markdown extension - Updated Python 3 only code to support Python 3

--- kpi/utils/gravatar_url.py
+++ kpi/utils/gravatar_url.py
@@ -12,9 +12,6 @@ def gravatar_url(email, https=True):
     return "%s://www.gravatar.com/avatar/%s?%s" % (
         'https' if https else 'http',
         hashlib.md5(hashable_str(email.lower())).hexdigest(),
-<<<<<<< HEAD
+
         urlencode({'s': '40'}),
-=======
-        urllib.urlencode({'s': '40'}),
->>>>>>> - Updated PIP dependencies to use latest Formpack commit for Python 3
         )
