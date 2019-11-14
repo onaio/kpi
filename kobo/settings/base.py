5351a2d115cd17e1a5d2006283119c383eb81dd3
Olivier Leger <olivierleger@gmail.com>
2019-11-14 11:37:22 -0400

    Use Django PostgreSQL native JSONField

--- kobo/settings/base.py
+++ kobo/settings/base.py
@@ -73,7 +73,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reversion',
-    'debug_toolbar',
     'mptt',
     'haystack',
     'private_storage',
@@ -118,10 +117,6 @@ MIDDLEWARE = [
     # 'django_userforeignkey.middleware.UserForeignKeyMiddleware',
     # 'django_request_cache.middleware.RequestCacheMiddleware',
 ]
-
-if DEBUG is True:
-    MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
-
 
 if os.environ.get('DEFAULT_FROM_EMAIL'):
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
