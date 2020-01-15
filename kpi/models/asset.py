bf58838c373e2b514374b406f1b67de906983bd2
Olivier Leger <olivierleger@gmail.com>
2020-01-15 12:53:09 -0400

    Added ToDo comments

--- kpi/models/asset.py
+++ kpi/models/asset.py
@@ -812,6 +812,7 @@ class Asset(ObjectPermissionMixin,
         if self.asset_type != ASSET_TYPE_COLLECTION:
             return None
 
+        # ToDo: See if using a loop can reduce the number of SQL queries.
         return self.permissions.filter(permission__codename=PERM_DISCOVER_ASSET,
                                        user_id=settings.ANONYMOUS_USER_ID).exists()
 
