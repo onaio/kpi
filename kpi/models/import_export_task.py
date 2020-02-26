622a97c81dab429605e09ea076f500b96425ef40
Olivier Leger <olivierleger@gmail.com>
2020-02-26 12:51:00 -0400

    Requested changes for PR#2578

--- kpi/models/import_export_task.py
+++ kpi/models/import_export_task.py
@@ -83,7 +83,6 @@ class ImportExportTask(models.Model):
     )
 
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    # ToDo validate whether both fields below need to be changed to jsonbfield
     data = JSONBField()
     messages = JSONBField(default=dict)
     status = models.CharField(choices=STATUS_CHOICES, max_length=32,
@@ -644,7 +643,7 @@ class ExportTask(ImportExportTask):
         self.remove_excess(self.user, source_url)
 
     @staticmethod
-    def _filter_by_source_kludge(queryset, source):
+    def filter_by_source(queryset, source):
         """
         A disposable way to filter a queryset by source URL.
         """
@@ -672,7 +671,7 @@ class ExportTask(ImportExportTask):
             user=user,
             date_created__lt=oldest_allowed_timestamp
         ).exclude(status__in=(cls.COMPLETE, cls.ERROR))
-        stuck_exports = cls._filter_by_source_kludge(stuck_exports, source)
+        stuck_exports = cls.filter_by_source(stuck_exports, source)
         for stuck_export in stuck_exports:
             logging.warning(
                 'Stuck export {}: type {}, username {}, source {}, '
@@ -697,7 +696,7 @@ class ExportTask(ImportExportTask):
 
         `source` is the source URL as included in the `data` attribute.
         """
-        user_source_exports = cls._filter_by_source_kludge(
+        user_source_exports = cls.filter_by_source(
             cls.objects.filter(user=user), source
         ).order_by('-date_created')
         excess_exports = user_source_exports[
