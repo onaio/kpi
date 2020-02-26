8d9dbf7f06ab8a34f8eb79db97ea9c51df8947bf
Olivier Leger <olivierleger@gmail.com>
2020-02-26 12:45:39 -0400

    refactored protected "_filter_by_source_kludge" to public "filter_by_source"

--- kpi/tests/test_mock_data_exports.py
+++ kpi/tests/test_mock_data_exports.py
@@ -480,7 +480,7 @@ class MockDataExports(TestCase):
             export_task.user = self.user
             export_task.data = task_data
             export_task.save()
-        created_export_tasks = ExportTask._filter_by_source_kludge(
+        created_export_tasks = ExportTask.filter_by_source(
             ExportTask.objects.filter(user=self.user),
             task_data['source']
         )
@@ -495,7 +495,7 @@ class MockDataExports(TestCase):
         self.assertFalse(result.storage.exists(result.name))
         self.assertListEqual( # assertSequenceEqual isn't working...
             list(export_tasks_to_keep.values_list('pk', flat=True)),
-            list(ExportTask._filter_by_source_kludge(
+            list(ExportTask.filter_by_source(
                 ExportTask.objects.filter(
                     user=self.user),
                 task_data['source']
@@ -509,7 +509,7 @@ class MockDataExports(TestCase):
         }
         self.assertEqual(
             0,
-            ExportTask._filter_by_source_kludge(
+            ExportTask.filter_by_source(
                 ExportTask.objects.filter(
                     user=self.user),
                 task_data['source']
@@ -526,7 +526,7 @@ class MockDataExports(TestCase):
             export_task.save()
         self.assertSequenceEqual(
             [ExportTask.CREATED, ExportTask.PROCESSING],
-            ExportTask._filter_by_source_kludge(
+            ExportTask.filter_by_source(
                 ExportTask.objects.filter(
                     user=self.user),
                 task_data['source']
@@ -541,7 +541,7 @@ class MockDataExports(TestCase):
         # Verify that the stuck exports have been marked
         self.assertSequenceEqual(
             [ExportTask.ERROR, ExportTask.ERROR, ExportTask.COMPLETE],
-            ExportTask._filter_by_source_kludge(
+            ExportTask.filter_by_source(
                 ExportTask.objects.filter(
                     user=self.user),
                 task_data['source']
