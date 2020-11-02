55cf53d7229687d6d3959e862a823aa5cd49adee
Olivier Leger <olivierleger@gmail.com>
2020-11-02 16:16:15 -0500

    Fixed tests

--- kpi/tests/api/v2/test_api_assets.py
+++ kpi/tests/api/v2/test_api_assets.py
@@ -125,7 +125,7 @@ class AssetsListApiTests(BaseAssetTestCase):
                     {
                         'name': 'zeppelin',
                         'type': 'select_one',
-                        'label': 'put on some zeppelin 🧀',
+                        'label': 'put on some zeppelin 🧀🧀🧀',
                         'select_from_list_name': 'choicelist',
                     }
                 ],
@@ -175,7 +175,7 @@ class AssetsListApiTests(BaseAssetTestCase):
         )
         self.assertListEqual(results, [template.uid, question.uid])
 
-        results = uids_from_search_results('🧀')
+        results = uids_from_search_results('🧀🧀🧀')
         self.assertListEqual(results, [template.uid])
 
         results = uids_from_search_results('pk:alrighty')
