d29561e7cd552525c84118c2464ec30455a2ce56
JacquelineMorrissette <jacquelinemariemorrissette@gmail.com>
2021-01-14 22:23:57 -0500

    Updated test_email.py to reflect REST Submission Email change

--- kobo/apps/hook/tests/test_email.py
+++ kobo/apps/hook/tests/test_email.py
@@ -53,6 +53,7 @@ class EmailTestCase(HookTestCase):
             "assets": {
                 self.asset.uid: {
                     "name": self.asset.name,
+                    "hook_uid": self.hook.uid,
                     "max_length": len(self.hook.name),
                     "logs": [{
                         "hook_name": self.hook.name,
